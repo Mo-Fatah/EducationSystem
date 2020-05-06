949a9c3dcbaaea2ad998dac327342b38c7df7982
Mohamed <mohamedabdelfatah2027@gmail.com>
2020-05-06 10:32:58 +0200

    finish the remaining methods

--- src/Assigment.java
+++ src/Assigment.java
@@ -6,10 +6,42 @@ public class Assigment {
     private Student student;
     private String content;
     private String solution;
-
+    private double grade;
     public Assigment(String name , Course course){
         this.name =name;
         this.course = course;
     }
+    // Used by Doctor
+    public void addContent(String content){
+        this.content = content;
+    }
+    // Used by Student
+    public void subitAns(String ans){
+        this.solution = ans;
+    }
+    // Used by TA
+    public void addGrade(double grade){
+        this.grade = grade;
+    }
 
+    public Course getCourse() {
+        return course;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+    
 }
