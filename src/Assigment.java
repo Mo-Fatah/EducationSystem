99eed14c8118a3b887a4112b9df7de07e643c57b
Mohamed <mohamedabdelfatah2027@gmail.com>
2020-05-06 10:24:46 +0200

    Create the constructor and add private fields

--- src/Assigment.java
+++ src/Assigment.java
@@ -1,2 +1,15 @@
 public class Assigment {
+    // the name of the Assignment
+    private String name;
+    // The Course of the Assignment
+    private Course course;
+    private Student student;
+    private String content;
+    private String solution;
+
+    public Assigment(String name , Course course){
+        this.name =name;
+        this.course = course;
+    }
+
 }
